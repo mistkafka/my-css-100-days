78b431c2022ef8789c90212c26139bfc77451642
mistkafka <mistkafka@gmail.com>
2019-05-23 23:10:43 +0800

    007: 看完参考答案后的感悟 + 修改

--- 007-notifications-search-and-menu/js/index.js
+++ 007-notifications-search-and-menu/js/index.js
@@ -2,13 +2,6 @@ const $topSearchInput = document.querySelector('#top-search-input');
 const $topSearchIcon = document.querySelector('#top-search-icon');
 $topSearchIcon.addEventListener('click', () => {
   $topSearchInput.classList.toggle('active');
-  if ($topSearchInput.classList.contains('show')) {
-    window.setTimeout(() => {
-      $topSearchInput.classList.remove('show');
-    }, 1000 * 0.4)
-  } else {
-    $topSearchInput.classList.add('show');
-  }
 });
 
 
