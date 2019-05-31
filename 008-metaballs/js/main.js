c6496dc46f4088fbff4f19cd8d26078db9e1ae53
mistkafka <zhenguo@glowing.com>
2019-05-31 09:52:49 +0800

    008, completed

--- 008-metaballs/js/main.js
+++ 008-metaballs/js/main.js
@@ -4,3 +4,9 @@ for (let i = 1; i <= 8; i++) {
   let deg = Math.floor(Math.random() * 300);
   $ball.style.transform = `rotate(${deg}deg)`;
 }
+
+for (let i = 1; i <= 10; i++) {
+  let $ball = document.querySelector(`.droplet-${i}`);
+  let deg = Math.floor(Math.random() * 300);
+  $ball.style.transform = `rotate(${deg}deg)`;
+}
